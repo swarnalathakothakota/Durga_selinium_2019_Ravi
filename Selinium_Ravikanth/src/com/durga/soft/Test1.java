3a494d437a5230040b7ad74f62286793f20742fc
swarnalathakothakota <56101572+swarnalathakothakota@users.noreply.github.com>
2019-10-06 22:27:10 -0700

    added sample demo project in github

---
+++ Selinium_Ravikanth/src/com/durga/soft/Test1.java
@@ -0,0 +1,13 @@
+package com.durga.soft;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		int a , b=0 , c=0;
+		a = b+ c;
+		
+	}
+
+}
